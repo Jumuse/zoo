25740509828f4d4a0df3d14bce91c5ce5b219df2
Jumuse <justinehoguet@gmail.com>
2024-10-25 23:40:52 +0200

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function number() : Response
+    {
+      $number = rand(0, 100);
+      return $this->render('base.html.twig', [
+                'number' => $number,
+      ]);
+    }
+}
